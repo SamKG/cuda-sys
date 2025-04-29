f70669d13fec73c29bd8ee696f0db4500db484cc
SamKG <samyak.k.gupta@gmail.com>
2025-04-29 13:17:09 -0400

    add struct

--- csrc/internal-fatbinary.h
+++ csrc/internal-fatbinary.h
@@ -1,1 +1,11 @@
 #include <fatbinary_section.h>
+
+typedef __attribute__((aligned(8))) struct fatBinaryHeader
+{
+    unsigned int           magic;
+    unsigned short         version;
+    unsigned short         headerSize;
+    unsigned long long int fatSize;
+} fatBinaryHeader;
+
+_Static_assert(_Alignof(fatBinaryHeader) == 8, "fatBinaryHeader must be 8-byte aligned");
